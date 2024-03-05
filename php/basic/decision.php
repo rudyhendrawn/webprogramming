21eb3041c5cd81f468794cfa05e724bc7cf6074c
rudyhend@gmail.com <rudyhend@gmail.com>
2024-03-05 11:36:20 +0800

    add basic, decision, and looping

---
+++ php/basic/decision.php
@@ -0,0 +1,52 @@
+<?php
+
+// Struktur Pengambilan Keputusan pada PHP
+
+// If-else
+echo "<h3>Struktur if-else</h3>";
+$num = 10;
+if ($num % 2 == 0)
+{
+    echo $num, " adalah Bilangan Genap";
+}
+else
+{
+    echo $num, " adalah Bilangan Ganjil";
+}
+
+echo "<br><br>";
+// Struktur If-else-if
+echo "<h3>Struktur if-else-if</h3>";
+$num = -10;
+if ($num > 0)
+{
+    echo $num, " adalah Bilangan Positif";
+}
+else if ($num < 0)
+{
+    echo $num, " adalah Bilangan Negatif";
+}
+else
+{
+    echo "adalah ", $num;
+}
+
+echo "<br><br>";
+// Struktur Switch-case
+echo "<h3>Struktur switch-case</h3>";
+$bulan = 1;
+switch ($bulan)
+{
+    case 1:
+        echo "Bulan ke-", $bulan, " adalah Januari";
+        break;
+    case 2:
+        echo "Bulan ke-", $bulan, " adalah Februari";
+        break;
+    case 3:
+        echo "Bulan ke-", $bulan, " adalah Maret";
+        break;
+    default:
+        echo "Tidak ada nomor bulan tersebut";
+        break;
+}
---
+++ php/basic/looping.php
@@ -0,0 +1,29 @@
+<?php
+
+echo "<h2>Struktur Pengulangan pada PHP</h2>";
+
+// Struktur for
+echo "<h3>Struktur For</h2>";
+for ($i=1; $i<=10; $i++)
+{
+    echo $i, " ";
+}
+
+// Struktur while-do
+echo "<h3>Struktur while-do</h2>";
+$i = 1;
+while ($i <= 10)
+{
+    echo $i, " ";
+    $i++;
+}
+
+// Struktur do-while
+echo "<h3>Struktur do-while</h2>";
+$i = 1;
+do
+{
+    echo $i, " ";
+    $i++;
+}
+while ($i <= 10);
